df9c43bc4303830291ef79912f6b03f87eb56507
Taliban Mkristu <taliban@Taliban>
2025-10-29 23:00:38 -0400

    Done with users serializer, views and task , users urls routing

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+from .serializers import UserSerializer, RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
